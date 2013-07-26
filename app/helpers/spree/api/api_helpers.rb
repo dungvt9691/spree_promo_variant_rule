fb85d76520f7cddef0257f1dda8fad41f9bf0390
Jason Reposa <jason@ad60.com>
2013-07-26 11:51:49 -0400

    Realized I didn't save my changes, so that code didn't actually work. I needed to use a decorator.

--- app/helpers/spree/api/api_helpers.rb
+++
@@ -1,9 +0,0 @@
-module Spree
-  module Api
-    module ApiHelpers
-      def variant_attributes
-        [:id, :name, :sku, :price, :weight, :height, :width, :depth, :is_master, :cost_price, :permalink, :options_text]
-      end
-    end
-  end
-end
